b0ded000b89493b7e39b9c8c7a9252e90623def0
PavanKalyan97 <42230095+PavanKalyan97@users.noreply.github.com>
2023-10-07 17:48:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,3 @@
-import streamlit
-
-streamlit.title('My Parents new healthy diner')
-
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
