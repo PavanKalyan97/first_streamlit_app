e4347690221811c7b363e487a2d6714d506bca8b
PavanKalyan97 <42230095+PavanKalyan97@users.noreply.github.com>
2023-10-07 17:44:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title('Education is important!')
+streamlit.title('My Parents new healthy diner')
 
-streamlit.header('Courses')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.text('Java')
-streamlit.text('SQL')
-streamlit.text('linux')
